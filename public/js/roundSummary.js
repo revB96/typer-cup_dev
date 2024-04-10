2c87ff9e2e3a9f1ede15d135be1d55e8553601d7
Querly96 <showpolska@gmail.com>
2024-04-10 21:37:02 +0200

    new file:   public/js/roundSummary copy.js 	modified:   public/js/roundSummary.js

--- public/js/roundSummary.js
+++ public/js/roundSummary.js
@@ -1,36 +1,45 @@
-function printRoundSummaryAccordion(){
-    $(`#round-summary-accordion`).html(`<div class="d-flex justify-content-center">
-        <div class="spinner-border" role="status">
-        <span class="visually-hidden">Loading...</span>
-        </div>
-    </div>`)
-    checkIfRoundIsOpen(getUserId()).then((roundState) => {
-        $(`#round-summary-accordion`).html("")
-        if(roundState == false){
-            getRound("running").then((runningRound) => {
-                getRoundTickets(runningRound[0].round).then(tickets =>{
-                    var ticketsByMatch = tickets.reduce((acc, value) => {
-                        if (!acc[value.schedule._id]) {
-                          acc[value.schedule._id] = [];
-                        }
-                        // Grouping
-                        acc[value.schedule._id].push(value);
-        
-                        return acc;
-                      }, {})
+function printRoundSummaryAccordion() {
+  getRound("running").then((runningRound) => {
+    getRoundTickets(runningRound[0].round).then((tickets) => {
+      var ticketsByMatch = tickets.reduce((acc, value) => {
+        if (!acc[value.schedule._id]) {
+          acc[value.schedule._id] = [];
+        }
+        // Grouping
+        acc[value.schedule._id].push(value);
 
-                      var ticketsByDateKeys = Object.keys(ticketsByMatch);
+        return acc;
+      }, {});
 
-                      ticketsByDateKeys.forEach(match => {
-                         
-                        var matchAccordion =`
+      var ticketsByDateKeys = Object.keys(ticketsByMatch);
+
+      ticketsByDateKeys.forEach((match) => {
+        var matchAccordion = `
                         <div class="accordion-item">
-                            <h2 style="text-align: center" class="accordion-header" id="flush-heading-${ticketsByMatch[match][0]._id}">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-${ticketsByMatch[match][0]._id}" aria-expanded="false" aria-controls="flush-collapse-${ticketsByMatch[match][0]._id}">
-                                ${ticketsByMatch[match][0].schedule.t1.teamName} <span class="flag-icon ml-1 mr-1 flag-icon-${ticketsByMatch[match][0].schedule.t1.shortcut.toLowerCase()}"></span> vs <span class="flag-icon ml-1 mr-1 flag-icon-${ticketsByMatch[match][0].schedule.t2.shortcut.toLowerCase()}"></span> ${ticketsByMatch[match][0].schedule.t2.teamName}
+                            <h2 style="text-align: center" class="accordion-header" id="flush-heading-${
+                              ticketsByMatch[match][0]._id
+                            }">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-${
+                              ticketsByMatch[match][0]._id
+                            }" aria-expanded="false" aria-controls="flush-collapse-${
+          ticketsByMatch[match][0]._id
+        }">
+                                ${
+                                  ticketsByMatch[match][0].schedule.t1.teamName
+                                } <span class="flag-icon ml-1 mr-1 flag-icon-${ticketsByMatch[
+          match
+        ][0].schedule.t1.shortcut.toLowerCase()}"></span> vs <span class="flag-icon ml-1 mr-1 flag-icon-${ticketsByMatch[
+          match
+        ][0].schedule.t2.shortcut.toLowerCase()}"></span> ${
+          ticketsByMatch[match][0].schedule.t2.teamName
+        }
                             </button>
                             </h2>
-                            <div id="flush-collapse-${ticketsByMatch[match][0]._id}" class="accordion-collapse collapse" aria-labelledby="flush-heading-${ticketsByMatch[match][0]._id}" data-bs-parent="#round-summary-accordion">
+                            <div id="flush-collapse-${
+                              ticketsByMatch[match][0]._id
+                            }" class="accordion-collapse collapse" aria-labelledby="flush-heading-${
+          ticketsByMatch[match][0]._id
+        }" data-bs-parent="#round-summary-accordion">
                             <div class="accordion-body">
                                 <div class="table-responsive">
                                     <table class="table table-sm" style="text-align: center;">
@@ -38,56 +47,72 @@ function printRoundSummaryAccordion(){
                                         <tr>
                                         <th scope="col">#</th>
                                         <th scope="col">Nick</th>
-                                        <th scope="col">${ticketsByMatch[match][0].schedule.t1.teamName}</th>
-                                        <th scope="col">${ticketsByMatch[match][0].schedule.t2.teamName}</th>
+                                        <th scope="col">${
+                                          ticketsByMatch[match][0].schedule.t1
+                                            .teamName
+                                        }</th>
+                                        <th scope="col">${
+                                          ticketsByMatch[match][0].schedule.t2
+                                            .teamName
+                                        }</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                        `
-                        var counter = 1
-                        ticketsByMatch[match].forEach(userTicket => {
-                            var nickname = userTicket.user.username;
-                            var textSize="";
-                            
-                            if(nickname.length > 12)
-                                textSize="font-size:11px;"
-                            
-                            matchAccordion += `
-                            <tr>
-                                <th scope="row">${counter}</th>
-                                <td>
-                                    <button style="border-style: none; background-color: transparent; ${textSize}" id="roundSummaryButton-${userTicket._id}">${userTicket.user.username}</button>
-                                    <script>tippy('#roundSummaryButton-${userTicket._id}', {
-                                        content: "${userTicket.user.friendlyName}",
-                                        placement: 'right-start',
-                                        theme: 'material',
-                                    });
-                                    </script>
-                                </td>
-                                <td>${userTicket.t1g}</td>
-                                <td>${userTicket.t2g}</td>
-                            </tr>
-                            `
-                            counter ++
-                        })
-                        matchAccordion += `
+                        `;
+
+        if (new Date() < new Date(ticketsByMatch[match][0].schedule.t1.matchDate))
+        {
+          var diff = Math.abs(new Date() - new Date(ticketsByMatch[match][0].schedule.t1.matchDate));
+          if (diff < 300000) {
+            matchAccordion = "Wysyłanie typów na mecz nadal trwa"
+          } else {
+            var counter = 1;
+            ticketsByMatch[match].forEach((userTicket) => {
+            var nickname = userTicket.user.username;
+            var textSize = "";
+
+            if (nickname.length > 12) textSize = "font-size:11px;";
+
+            matchAccordion += `
+                                <tr>
+                                    <th scope="row">${counter}</th>
+                                    <td>
+                                        <button style="border-style: none; background-color: transparent; ${textSize}" id="roundSummaryButton-${userTicket._id}">${userTicket.user.username}</button>
+                                        <script>tippy('#roundSummaryButton-${userTicket._id}', {
+                                            content: "${userTicket.user.friendlyName}",
+                                            placement: 'right-start',
+                                            theme: 'material',
+                                        });
+                                        </script>
+                                    </td>
+                                    <td>${userTicket.t1g}</td>
+                                    <td>${userTicket.t2g}</td>
+                                </tr>
+                                `;
+            counter++;
+            });
+          }
+        }
+        if (new Date() > new Date(match.matchDate)) {
+          matchAccordion = "Wysyłanie typów na mecz nadal trwa"
+        }
+
+        matchAccordion += `
                                         </tbody>
                                     </table>
                                 </div>
                             </div>
                             </div>
                         </div>
-                        `
-                        $(`#round-summary-accordion`).append(matchAccordion)
-                      })
-                })
-            })
-        }
-    })
+                        `;
+        $(`#round-summary-accordion`).append(matchAccordion);
+      });
+    });
+  });
 }
 
 $(document).ready(function () {
-    if(document.title == "Typer Cup-DEV | Podsumowanie rundy"){
-        printRoundSummaryAccordion()
-    }
-})
+  if (document.title == "Typer Cup-DEV | Podsumowanie rundy") {
+    printRoundSummaryAccordion();
+  }
+});
