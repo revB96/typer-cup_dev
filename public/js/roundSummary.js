b1efa5b7422bcef081d9be41652d795776aaee4d
Querly96 <showpolska@gmail.com>
2024-04-10 22:26:47 +0200

    modified:   public/js/roundSummary.js

--- public/js/roundSummary.js
+++ public/js/roundSummary.js
@@ -14,86 +14,57 @@ function printRoundSummaryAccordion() {
       var ticketsByDateKeys = Object.keys(ticketsByMatch);
 
       ticketsByDateKeys.forEach((match) => {
-        if (
-          new Date() < new Date(ticketsByMatch[match][0].schedule.matchDate)
-        ) {
-          var diff = Math.abs(
-            new Date() - new Date(ticketsByMatch[match][0].schedule.matchDate)
-          );
-          if (diff < 300000) {
-            var matchAccordion = `
-                        <div class="accordion-item">
-                            <h2 style="text-align: center" class="accordion-header" id="flush-heading-${
-                              ticketsByMatch[match][0]._id
-                            }">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-${
-                              ticketsByMatch[match][0]._id
-                            }" aria-expanded="false" aria-controls="flush-collapse-${
-              ticketsByMatch[match][0]._id
-            }">
-                                ${
-                                  ticketsByMatch[match][0].schedule.t1.teamName
-                                } <span class="flag-icon ml-1 mr-1 flag-icon-${ticketsByMatch[
-              match
-            ][0].schedule.t1.shortcut.toLowerCase()}"></span> vs <span class="flag-icon ml-1 mr-1 flag-icon-${ticketsByMatch[
-              match
-            ][0].schedule.t2.shortcut.toLowerCase()}"></span> ${
-              ticketsByMatch[match][0].schedule.t2.teamName
+        ticketsByMatch[match].forEach((userTicket) => {
+            if (new Date() < new Date(ticketsByMatch[match][0].schedule.matchDate)) {
+                var diff = Math.abs(new Date() - new Date(ticketsByMatch[match][0].schedule.matchDate));
+                if (diff < 300000){
+                    var counter = 1;
+                    var matchAccordion = `
+                    <div class="accordion-item">
+                        <h2 style="text-align: center" class="accordion-header" id="flush-heading-${ticketsByMatch[match][0]._id}">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-${ticketsByMatch[match][0]._id}" aria-expanded="false" aria-controls="flush-collapse-${ticketsByMatch[match][0]._id}">
+                            ${ticketsByMatch[match][0].schedule.t1.teamName} <span class="flag-icon ml-1 mr-1 flag-icon-${ticketsByMatch[match][0].schedule.t1.shortcut.toLowerCase()}"></span> vs <span class="flag-icon ml-1 mr-1 flag-icon-${ticketsByMatch[match][0].schedule.t2.shortcut.toLowerCase()}"></span> ${ticketsByMatch[match][0].schedule.t2.teamName}
+                        </button>
+                        </h2>
+                        <div id="flush-collapse-${ticketsByMatch[match][0]._id}" class="accordion-collapse collapse" aria-labelledby="flush-heading-${ticketsByMatch[match][0]._id}" data-bs-parent="#round-summary-accordion">
+                        <div class="accordion-body">
+                            <div class="table-responsive">
+                                <table class="table table-sm" style="text-align: center;">
+                                <thead class="table-dark">
+                                    <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Nick</th>
+                                    <th scope="col">${ticketsByMatch[match][0].schedule.t1.teamName}</th>
+                                    <th scope="col">${ticketsByMatch[match][0].schedule.t2.teamName}</th>
+                                    </tr>
+                                </thead>
+                                <tbody>`;
+                    ticketsByMatch[match].forEach(userTicket => {
+                        var nickname = userTicket.user.username;
+                        var textSize = "";
+                        if (nickname.length > 12) textSize = "font-size:11px;";
+                        matchAccordion += `
+                        <tr>
+                            <th scope="row">${counter}</th>
+                            <td>
+                                <button style="border-style: none; background-color: transparent; ${textSize}" id="roundSummaryButton-${userTicket._id}">${userTicket.user.username}</button>
+                                <script>tippy('#roundSummaryButton-${userTicket._id}', {
+                                    content: "${userTicket.user.friendlyName}",
+                                    placement: 'right-start',
+                                    theme: 'material',
+                                });
+                                </script>
+                            </td>
+                            <td>${userTicket.t1g}</td>
+                            <td>${userTicket.t2g}</td>
+                        </tr>`;
+
+                        counter++;
+
+                    })
+                }
             }
-                            </button>
-                            </h2>
-                            <div id="flush-collapse-${
-                              ticketsByMatch[match][0]._id
-                            }" class="accordion-collapse collapse" aria-labelledby="flush-heading-${
-              ticketsByMatch[match][0]._id
-            }" data-bs-parent="#round-summary-accordion">
-                            <div class="accordion-body">
-                                <div class="table-responsive">
-                                    <table class="table table-sm" style="text-align: center;">
-                                    <thead class="table-dark">
-                                        <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Nick</th>
-                                        <th scope="col">${
-                                          ticketsByMatch[match][0].schedule.t1
-                                            .teamName
-                                        }</th>
-                                        <th scope="col">${
-                                          ticketsByMatch[match][0].schedule.t2
-                                            .teamName
-                                        }</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                        `;
-
-            var counter = 1;
-            ticketsByMatch[match].forEach((userTicket) => {
-              var nickname = userTicket.user.username;
-              var textSize = "";
-
-              if (nickname.length > 12) textSize = "font-size:11px;";
-
-              matchAccordion += `
-                                <tr>
-                                    <th scope="row">${counter}</th>
-                                    <td>
-                                        <button style="border-style: none; background-color: transparent; ${textSize}" id="roundSummaryButton-${userTicket._id}">${userTicket.user.username}</button>
-                                        <script>tippy('#roundSummaryButton-${userTicket._id}', {
-                                            content: "${userTicket.user.friendlyName}",
-                                            placement: 'right-start',
-                                            theme: 'material',
-                                        });
-                                        </script>
-                                    </td>
-                                    <td>${userTicket.t1g}</td>
-                                    <td>${userTicket.t2g}</td>
-                                </tr>
-                                `;
-              counter++;
-            });
-          }
-          matchAccordion += `
+        matchAccordion += `
                                         </tbody>
                                     </table>
                                 </div>
@@ -101,12 +72,14 @@ function printRoundSummaryAccordion() {
                             </div>
                         </div>
                         `;
-          $(`#round-summary-accordion`).append(matchAccordion);
-        }
-      });
-    });
-  });
+        $(`#round-summary-accordion`).append(matchAccordion);
+
+        })
+    })
+    })
+  })
 }
+ 
 
 $(document).ready(function () {
   if (document.title == "Typer Cup-DEV | Podsumowanie rundy") {
