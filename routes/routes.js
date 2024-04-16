4b747eb3e739cbd2de611f8900e042e17ddb9161
Querly96 <showpolska@gmail.com>
2024-04-17 00:07:41 +0200

    modified:   routes/routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -52,7 +52,7 @@ router.get("/table", authenticate, async function (req, res) {
 
 router.get("/euro", authenticate, async function (req, res) {
   res.render("euro", {
-    title: "Mistrzostwa Świata 2022",
+    title: req.cookies.edition,
     activeEdition: req.cookies.edition,
     lastRound: 1
 });
