bfb6a38cb0f45eb9a9592a3ab8fc46f41be8607a
Querly96 <showpolska@gmail.com>
2024-04-16 22:55:16 +0200

    modified:   public/js/rules.js 	modified:   views/rules.handlebars

--- public/js/rules.js
+++ public/js/rules.js
@@ -1,16 +1,52 @@
-function printRules() {  
+function printRegulationsRules() {  
     getRegulationsBySection("rules").then(async (rules) => {
       $(`#regulations-list-rules`).html("");
-      rules.forEach(async (rule, index) => {
+      rules.forEach(async (result, index) => {
         await $(`#regulations-list-rules`).append(`
-        <li class="list-group-item ${rule.color}">${rule.content}</li>
+        <li class="list-group-item ${result.color}">${result.content}</li>
         `);
       });
     });
-  }
+}
+
+function printRegulationsPoints() {  
+    getRegulationsBySection("points").then(async (points) => {
+      $(`#regulations-list-points`).html("");
+      points.forEach(async (result, index) => {
+        await $(`#regulations-list-rules`).append(`
+        <li class="list-group-item ${result.color}">${result.content}</li>
+        `);
+      });
+    });
+}
+
+function printRegulationsAwards() {  
+    getRegulationsBySection("awards").then(async (awards) => {
+      $(`#regulations-list-awards`).html("");
+      awards.forEach(async (result, index) => {
+        await $(`#regulations-list-awards`).append(`
+        <li class="list-group-item ${result.color}">${result.content}</li>
+        `);
+      });
+    });
+}
+
+function printRegulationsSiteMap() {  
+    getRegulationsBySection("site_map").then(async (site_map) => {
+      $(`#regulations-list-site_map`).html("");
+      site_map.forEach(async (result, index) => {
+        await $(`#regulations-list-awards`).append(`
+        <li class="list-group-item ${result.color}">${result.content}</li>
+        `);
+      });
+    });
+}
 
 $(document).ready(function () {
     if (document.title == "Typer Cup-DEV | Regulamin i zasady") {
-        printRules()
+        printRegulationsRules()
+        printRegulationsPoints()
+        printRegulationsAwards()
+        printRegulationsSiteMap()
     }
   });
